f87384e5a53a451d4c26e5cef038e2bbdbb51d78
Nadia <nadia.matulewicz@gmail.com>
2024-12-03 21:25:52 +0100

    Solve Day 2 part 1

--- AOC.ConsoleApp/Program.cs
+++ AOC.ConsoleApp/Program.cs
@@ -1,11 +1,11 @@
 ﻿using AOC.ConsoleApp.Solvers;
 
-ISolver solver = new Day02Solver();
+ISolver solver = new Day03Solver();
 
-var linesFirstChallenge = ReadLines("../../../Inputs/02_01.txt");
+var linesFirstChallenge = ReadLines("../../../Inputs/03_01.txt");
 Console.WriteLine($"First solution: {solver.SolveFirstChallenge(linesFirstChallenge)}");
 
-var linesSecondChallenge = ReadLines("../../../Inputs/02_01.txt");
+var linesSecondChallenge = ReadLines("../../../Inputs/03_01.txt");
 Console.WriteLine($"Second solution: {solver.SolveSecondChallenge(linesSecondChallenge)}");
 
 static IEnumerable<string> ReadLines(string fileName)
---
+++ AOC.ConsoleApp/Solvers/Day03Solver.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AOC.ConsoleApp.Solvers;
+
+public class Day03Solver : ISolver
+{
+    public string SolveFirstChallenge(IEnumerable<string> lines)
+    {
+        var completeInput = lines.Aggregate((line1, line2) => line1 + line2);
+        
+        IEnumerable<string> validMultiplicationStrings = FindValidMultiplications(completeInput);
+        IEnumerable<(int, int)> validMultiplications = validMultiplicationStrings.Select(GetMultiplicationFactors);
+
+        return validMultiplications.Select(factors => factors.Item1 * factors.Item2).Sum().ToString();
+    }
+
+    public string SolveSecondChallenge(IEnumerable<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    private IEnumerable<string> FindValidMultiplications(string input)
+    {
+        var pattern = @"mul\([0-9]+,[0-9]+\)";
+        var matches = Regex.Matches(input, pattern);
+        return matches.Select(match => match.Value);
+    }
+
+    private (int, int) GetMultiplicationFactors(string multiplicationString)
+    {
+        var pattern = @"[0-9]+";
+
+        var matches = Regex.Matches(multiplicationString, pattern);
+        if (matches.Count != 2)
+            throw new Exception($"Something went wrong. " +
+                $"Exactly two numbers were expected in '{multiplicationString}', but {matches.Count} matches were found.");
+
+        return (int.Parse(matches[0].Value), int.Parse(matches[1].Value));
+    }
+}
