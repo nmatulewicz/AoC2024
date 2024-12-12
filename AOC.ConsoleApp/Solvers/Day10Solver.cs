ec988e5c723665bf362f968668c85c5f27edf7ba
Nadia <nadia.matulewicz@gmail.com>
2024-12-12 19:54:49 +0100

    start on Day 10 part 1

---
+++ AOC.ConsoleApp/Solvers/Day10Solver.cs
@@ -0,0 +1,20 @@
+﻿
+namespace AOC.ConsoleApp.Solvers;
+
+public class Day10Solver : AbstractSolver
+{
+
+    public Day10Solver(IEnumerable<string> lines) : base(lines)
+    {
+    }
+
+    public override string SolveFirstChallenge()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string SolveSecondChallenge()
+    {
+        throw new NotImplementedException();
+    }
+}
