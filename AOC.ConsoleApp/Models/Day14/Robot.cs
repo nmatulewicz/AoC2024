b9479e04b6d44f309b941e4af600125bc8304d6b
Nadia <nadia.matulewicz@gmail.com>
2024-12-21 08:13:24 +0100

    add input for day 14

---
+++ AOC.ConsoleApp/Models/Day14/Robot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC.ConsoleApp.Models.Day14;
+
+public class Robot
+{
+}
