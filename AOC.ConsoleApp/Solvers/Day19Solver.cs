9590ceada7bd598d1338a2b459476ab22060ae9a
Nadia <nadia.matulewicz@gmail.com>
2024-12-26 11:07:29 +0100

    solve day 19 part 2

--- AOC.ConsoleApp/Solvers/Day19Solver.cs
+++ AOC.ConsoleApp/Solvers/Day19Solver.cs
@@ -6,11 +6,13 @@ public class Day19Solver : AbstractSolver
 {
     private IEnumerable<string> _towels;
     private IEnumerable<string> _designs;
+    private IDictionary<string, long> _numberOfWaysDictionary;
 
     public Day19Solver(IEnumerable<string> lines) : base(lines)
     {
         _towels = GetTowels();
         _designs = GetDesigns();
+        _numberOfWaysDictionary = new Dictionary<string, long>();
     }
 
     public override string SolveFirstChallenge()
@@ -24,8 +26,13 @@ public class Day19Solver : AbstractSolver
     }
 
     public override string SolveSecondChallenge()
-    { 
-        throw new NotImplementedException();
+    {
+        long totalNumberOfWays = 0;
+        foreach (var design in _designs)
+        {
+            totalNumberOfWays += NumberOfWays(design);
+        }
+        return totalNumberOfWays.ToString();
     }
 
     private bool IsPossible(string design)
@@ -38,6 +45,21 @@ public class Day19Solver : AbstractSolver
                 return true;
         }
         return false;
+    }
+
+    private long NumberOfWays(string design)
+    {
+        if (string.IsNullOrEmpty(design)) return 1;
+        if (_numberOfWaysDictionary.TryGetValue(design, out var numberOfWays)) return numberOfWays;
+
+        numberOfWays = 0;
+        foreach (var towel in _towels)
+        {
+            if (design.StartsWith(towel)) numberOfWays += NumberOfWays(design.Substring(towel.Length));
+        }
+        _numberOfWaysDictionary.Add(design, numberOfWays);
+
+        return numberOfWays;
     }
 
     private IEnumerable<string> GetDesigns()
