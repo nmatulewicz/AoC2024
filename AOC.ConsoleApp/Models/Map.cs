fd09ea48b2c9110c18ddfcf6eafa0d8af100c4a9
Nadia <nadia.matulewicz@gmail.com>
2024-12-28 13:37:27 +0100

    refactor

--- AOC.ConsoleApp/Models/Map.cs
+++ AOC.ConsoleApp/Models/Map.cs
@@ -19,27 +19,39 @@ public class Map
 
     public int GetLengthShortestPath()
     {
-        var start = _map.Where(position => position.Value ==  START).First();
+        return GetShortestPath().Count() - 1;
+    }
+
+    public IEnumerable<GridPosition<char>> GetWalls()
+    {
+        return _map.Where(position => position.Value == WALL);
+    }
+
+    public IEnumerable<GridPosition<char>> GetShortestPath()
+    {
+        var start = _map.Where(position => position.Value == START).First();
         var end = _map.Where(position => position.Value == END).First();
 
         var priorityQueue = new PriorityQueue<GridPosition<char>, int>();
         priorityQueue.Enqueue(start, 0);
 
-        var bestLengthDictionary = _map.ToDictionary(position => position, position => int.MaxValue);
-        bestLengthDictionary[start] = 0;
+        var bestLengthDictionary = _map.ToDictionary<GridPosition<char>, GridPosition<char>, (int bestLength, GridPosition<char>? previousPosition)>(
+            position => position, 
+            position => (int.MaxValue, null));
+        bestLengthDictionary[start] = (0, null);
 
         while (priorityQueue.TryDequeue(out var position, out var pathLength))
         {
-            if (position.Equals(end)) return pathLength;
+            if (position.Equals(end)) return DeterminePath(end, bestLengthDictionary);
 
             var neighbours = position.GetAllDirectNeighbours().Where(neighbour => neighbour.Value != WALL);
             foreach (var neighbour in neighbours)
             {
-                var currentPathLength = bestLengthDictionary[neighbour];
+                var currentPathLength = bestLengthDictionary[neighbour].bestLength;
                 var newPathLength = pathLength + 1;
                 if (newPathLength >= currentPathLength) continue;
 
-                bestLengthDictionary[neighbour] = newPathLength;
+                bestLengthDictionary[neighbour] = (newPathLength, position);
                 priorityQueue.Enqueue(neighbour, newPathLength);
             }
         }
@@ -47,8 +59,20 @@ public class Map
         throw new Exception("Queue should not be empty before reaching end.");
     }
 
-    public IEnumerable<GridPosition<char>> GetWalls()
+    private IEnumerable<GridPosition<char>> DeterminePath(GridPosition<char> end, Dictionary<GridPosition<char>, (int bestLength, GridPosition<char>? previousPosition)> bestLengthDictionary)
     {
-        return _map.Where(position => position.Value == WALL);
+        var bestPathLength = bestLengthDictionary[end].bestLength;
+        var bestPathArray = new GridPosition<char>[bestPathLength + 1];
+
+        var currentPosition = end;
+        bestPathArray[bestPathLength] = currentPosition;
+        while (bestLengthDictionary[currentPosition] is (int bestLength, GridPosition<char> previousPosition))
+        {
+            bestPathArray[bestLength] = currentPosition;
+            currentPosition = previousPosition;
+        }
+
+        bestPathArray[0] = currentPosition;
+        return bestPathArray;
     }
 }
