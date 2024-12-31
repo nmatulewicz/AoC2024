8f653d0a7aee5cbb29d35cffee4abcdf4ce59e29
Nadia <nadia.matulewicz@gmail.com>
2024-12-31 16:31:23 +0100

    solve day  22 part 1

--- AOC.ConsoleApp/Models/Day22/SecretNumber.cs
+++ AOC.ConsoleApp/Models/Day22/SecretNumber.cs
@@ -1,5 +1,36 @@
-﻿namespace AOC.ConsoleApp.Models.Day22;
+﻿
+namespace AOC.ConsoleApp.Models.Day22;
 
 public class SecretNumber
 {
+    public readonly long Value;
+    public SecretNumber(long value)
+    {
+        Value = value;
+    }
+
+    public SecretNumber MixWith(SecretNumber other)
+    {
+        return new SecretNumber(Value ^ other.Value);
+    }
+
+    public SecretNumber Prune()
+    {
+        return new SecretNumber(Value & (16777216 - 1));
+    }
+
+    public static SecretNumber operator <<(SecretNumber left, int right)
+    {
+        return new SecretNumber(left.Value << right);
+    }
+
+    public static SecretNumber operator >>(SecretNumber left, int right)
+    {
+        return new SecretNumber(left.Value >> right);
+    }
+
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
 }
--- AOC.ConsoleApp/Solvers/Day22Solver.cs
+++ AOC.ConsoleApp/Solvers/Day22Solver.cs
@@ -1,14 +1,28 @@
-﻿namespace AOC.ConsoleApp.Solvers;
+﻿using AOC.ConsoleApp.Models.Day22;
+
+namespace AOC.ConsoleApp.Solvers;
 
 public class Day22Solver : AbstractSolver
 {
+    private IEnumerable<SecretNumber> _secretNumbers;
     public Day22Solver(IEnumerable<string> lines) : base(lines)
     {
+        _secretNumbers = _lines.Select(long.Parse).Select(number => new SecretNumber(number));
     }
 
     public override string SolveFirstChallenge()
     {
-        throw new NotImplementedException();
+        IEnumerable<SecretNumber> secrets = _secretNumbers;
+        for (var i = 0; i < 2000; i++)
+        {
+            var results = secrets
+                .Select(secret => secret.MixWith(secret << 6).Prune())
+                .Select(secret => secret.MixWith(secret >> 5).Prune())
+                .Select(secret => secret.MixWith(secret << 11).Prune());
+            secrets = results;
+        }
+        return secrets.Sum(secret => secret.Value).ToString();
+        // 17347641344 ==> Too high
     }
 
     public override string SolveSecondChallenge()
